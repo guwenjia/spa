507188b4faf6097013c580a15ba7bc265d0596ad
谷文佳 <1023478561@qq.com>
2018-03-15 18:54:50 +0800

    Update index.js

--- 03-test3/index.js
+++ 03-test3/index.js
@@ -8,25 +8,28 @@ $(function(){
            $("#one").html("<br/>输入不能为空！");
            $("#one").css("color","red");
            $("#one").css("font-size","12px");
-           $("#calc").attr("disabled",true);//不能点击计算按钮
+          
            
           }
           else if ($("#width").val()< 0) {
            $("#one").html("<br/>宽度必须大于零");
            $("#one").css("color","red");
            $("#one").css("font-size","12px");
-           $("#calc").attr("disabled",true); 
+           
            }
           else if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-?\d+])?$/.test($("#width").val())){
            // console.log("111");
            $("#one").html("<br/>宽度必须是数值");
            $("#one").css("color","red");
            $("#one").css("font-size","12px");
-           $("#calc").attr("disabled",true);
+           
           }
           else{
             $("#one").html("");
-            $("#calc").attr("disabled",false);
+             var w = $("#width").val();
+             var h = $("#height").val();
+             $("#perimeter").val(w*2+h*2);
+            $("#area").val(w*h);
           }
        
       
@@ -41,25 +44,25 @@ $(function(){
              $("#two").html("<br/>长度必须大于零");
              $("#two").css("color","red");
              $("#two").css("font-size","12px");
-            // $("#calc").attr("disabled",true); 
+            
           }
           else if(!/^-?(0|[1-9]\d*)(\.\d*)?([eE][+-?\d+])?$/.test($("#height").val())){
-           // console.log("111");
+           
            $("#two").html("<br/>长度必须是数值");
            $("#two").css("color","red");
            $("#two").css("font-size","12px");
-           //$("#calc").attr("disabled",true);
+           
           }
           else{
             $("#two").html("");
-            $("#calc").attr("disabled",false);
+            var w = $("#width").val();
+            var h = $("#height").val();
+             $("#perimeter").val(w*2+h*2);
+             $("#area").val(w*h);
           }
       
 
-     var w = $("#width").val();
-     var h = $("#height").val();
-     $("#perimeter").val(w*2+h*2);
-     $("#area").val(w*h);
+
 
 
   });
